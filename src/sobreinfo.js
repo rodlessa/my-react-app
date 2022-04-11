70194278951a6e962129955a85545105e48ce8f2
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2022-04-11 15:36:59 -0300

    update pages

--- src/sobreinfo.js
+++ src/sobreinfo.js
@@ -2,7 +2,7 @@ function SobreInfo(){
     return(
         <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
       <main className="p-2">
-      <div className="container p-5 text-white rounded-3 bg-black">
+      <div className="container p-5 text-white rounded-3 bg-preto">
       <p class="text-center">
       Desenvolvedor Full-stack focado em experiencia de usuario, compatibilidade multi-device e utilização de recursos. </p>
         </div>
