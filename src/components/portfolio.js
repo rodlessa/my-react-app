d7001e5b66a04f4e983b834dce322f3db4178bf0
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2022-06-27 09:33:43 -0300

    update new visual

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -4,20 +4,21 @@
 <div class="container ">
     <div class="row">
     <div class="col sample text-center">
-        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/album.png" class="img-thumbnail" width="300px" />
+        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/album.png" class="img-thumbnail" />
       
             <p class="p-2">Seu conteudo.</p>
     </div>
     <div class="col sample text-center">
-        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/cover.png" class="img-thumbnail" width="300px" />
+        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/cover.png" class="img-thumbnail" />
 
             <p class="p-2">Sua identidade.</p>
     </div>
     <div class="col sample container text-center">
-        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/product.png" class="img-thumbnail" width="300px" />
+        <img src="https://getbootstrap.com/docs/4.0/examples/screenshots/product.png" class="img-thumbnail" />
 
         <p class="p-2">Página perfeita para o seu produto.</p>
     </div>
+    
 </div>
 </div>
 </div>
